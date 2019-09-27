05e29cdea71887cabfc9ec392ccb8a7e5e97664d
Uilian Ries <uilianries@gmail.com>
2019-09-27 18:54:27 -0300

    Fix unit tests
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     #entry_points={
     #    'console_scripts': [
-    #        'patch.py=patch',
+    #        'patch_ng.py=patch',
     #    ],
     #},
 )
