3437668b00c6167dabfcb9402d0b7c7070237241
Uilian Ries <uilianries@gmail.com>
2019-10-24 11:21:48 -0300

    #5 Validate fuzzy patch
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -424,6 +424,12 @@ class TestPatchApply(unittest.TestCase):
         pto.apply(strip=0, root=treeroot)
         self.assertFalse(os.path.exists(os.path.join(treeroot, 'deleted')))
 
+    def test_fuzzy_patch(self):
+        treeroot = join(self.tmpdir, 'rootparent')
+        shutil.copytree(join(TESTS, '10fuzzy'), treeroot)
+        pto = patch_ng.fromfile(join(TESTS, '10fuzzy/10fuzzy.patch'))
+        self.assertTrue(pto.apply(root=treeroot))
+
 
 class TestHelpers(unittest.TestCase):
     # unittest setting
