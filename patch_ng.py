7501539e6bc6a31083a32dda2eb277d7b31aab5e
Javier G. Sogo <jgsogo@gmail.com>
2020-03-05 11:56:51 +0100

    bump version

--- patch_ng.py
+++ patch_ng.py
@@ -31,7 +31,7 @@
 from __future__ import print_function
 
 __author__ = "Conan.io <info@conan.io>"
-__version__ = "1.17.2"
+__version__ = "1.17.3"
 __license__ = "MIT"
 __url__ = "https://github.com/conan-io/python-patch"
 
