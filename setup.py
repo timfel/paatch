885476a6e07048159654b8559cfbb2a6c7e28d96
James <memsharded@gmail.com>
2024-10-25 14:01:46 +0200

    added pyproject (#36)

--- setup.py
+++ setup.py
@@ -75,6 +75,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
 
     # What does your project relate to?
