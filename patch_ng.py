65887e01a1747ea61559ce15340789a508f73c36
Tim Felgentreff <tim.felgentreff@oracle.com>
2024-10-28 09:16:48 +0100

    Support -i option like GNU patch

--- patch_ng.py
+++ patch_ng.py
@@ -1303,7 +1303,8 @@ def main():
 
   opt = OptionParser(usage="1. %prog [options] unified.diff\n"
                     "       2. %prog [options] http://host/patch\n"
-                    "       3. %prog [options] -- < unified.diff",
+                    "       3. %prog [options] -- < unified.diff"
+                    "       4. %prog [options] -i unified.diff",
                      version="python-patch %s" % __version__)
   opt.add_option("-q", "--quiet", action="store_const", dest="verbosity",
                                   const=0, help="print only warnings and errors", default=1)
@@ -1319,13 +1320,15 @@ def main():
   opt.add_option("--revert", action="store_true",
                                            help="apply patch in reverse order (unpatch)")
   opt.add_option("-f", "--fuzz", action="store_true", dest="fuzz", help="Accept fuuzzy patches")
+  opt.add_option("-i", "--input", metavar='PATCHFILE',
+                                           help="Read patch from PATCHFILE instead of stdin.")
   (options, args) = opt.parse_args()
 
-  if not args and sys.argv[-1:] != ['--']:
+  if not args and not options.input and sys.argv[-1:] != ['--']:
     opt.print_version()
     opt.print_help()
     sys.exit()
-  readstdin = (sys.argv[-1:] == ['--'] and not args)
+  readstdin = (sys.argv[-1:] == ['--'] and not args and not options.input)
 
   verbosity_levels = {0:logging.WARNING, 1:logging.INFO, 2:logging.DEBUG}
   loglevel = verbosity_levels[options.verbosity]
@@ -1339,7 +1342,7 @@ def main():
   if readstdin:
     patch = PatchSet(sys.stdin)
   else:
-    patchfile = args[0]
+    patchfile = options.input if options.input else args[0]
     urltest = patchfile.split(':')[0]
     if (':' in patchfile and urltest.isalpha()
         and len(urltest) > 1): # one char before : is a windows drive letter
