e27cb2d45cf9acf9c15bfb546fdd3ccfb4dd44d8
Tim Felgentreff <tim.felgentreff@oracle.com>
2025-03-25 12:34:04 +0100

    Add patch.exe as entry point

--- setup.py
+++ setup.py
@@ -120,9 +120,9 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'patch_ng.py=patch',
-    #    ],
-    #},
+    entry_points={
+       'console_scripts': [
+           'patch = patch_ng:main',
+       ],
+    },
 )
